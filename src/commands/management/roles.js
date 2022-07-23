adcb02644e7891c96bc452374c3c9ae3cf75eb74
Synol_ <louisvaillant44@gmail.com>
2022-07-23 13:09:09 +0200

    roles command

--- src/commands/management/roles.js
+++ src/commands/management/roles.js
@@ -6,7 +6,7 @@ module.exports = {
     type: 1,
     description: 'Create a message role management',
     defer: false,
-    ephemeral: true,
+    ephemeral: false,
     default_permission: false,
     options: [
         /*{
@@ -43,33 +43,43 @@ module.exports = {
      * @param {CommandInteractionOptionResolver} options Application Command Options
      */
     run: async function (client, interaction, options) {
+
         const embed = new MessageEmbed()
             .setTitle("    -- Choix des rôles --")
             .setURL("https://github.com/Synol14/babar_o_bot")
             .setDescription("Il suffit simplement d'ajouter une réaction correspondant à vos rôles.")
             .addField('Obligatoire :', `🥇 - 1ère année\n🥈 - 2ème année\n${interaction.guild.emojis.cache.find(emoji => emoji.name === 'Peip')} - Peip `)
-            .addField('Pour les premières années :', ':regional_indicator_a: - TD A\n:regional_indicator_b: - TD B\n:regional_indicator_c: - TD C\n:regional_indicator_d: - TD D')
-            .addField('Autres rôles :', `🍺 - Fétard\n🏨 - Espacil\n:man_detective: - Externe`)
+            .addField('Pour les premières années :', `:regional_indicator_a: - TD A\n:regional_indicator_b: - TD B\n:regional_indicator_c: - TD C\n:regional_indicator_d: - TD D`)
+            .addField('Pour les deuxièmes années :', `${interaction.guild.emojis.cache.find(emoji => emoji.name === 'ESE')} - ESE\n${interaction.guild.emojis.cache.find(emoji => emoji.name === 'AII')} - AII\n⚡ - EME\n👨‍🏭 - Alternant`)
+            .addField('Autres rôles :', `🍺 - Fétard\n${interaction.guild.emojis.cache.find(emoji => emoji.name === 'Sportif')} - Sportif\n$🎵 - Musicien\n🏨 - Espacil\n🕵️‍♂️ - Externe`)
             .addField('Jeux :', "Il y a un émoji par jeu. (d'autres pourront arriver)")
             .setThumbnail("https://cdn.discordapp.com/attachments/747195335239008307/761304615710752768/logo-GEII.png"/*interaction.guild.iconURL()*/)
             .setColor(getBotColor(client, interaction.guildId))
             .setImage("https://images-ext-2.discordapp.net/external/aRxrlHKNog3J6POqtXEoT_mv77uDMjZLIpNX2oglFgA/https/imgur.com/sRly2m6.png")
 
-        interaction.reply({ content: 'Voilà !', ephemeral: true });
+        interaction.deferReply();
 
         try {
             interaction.channel.send({ embeds: [embed] }).then(message => {
                 message?.react('🥇');
                 message?.react('🥈');
+                message?.react(message.guild.emojis.cache.find(emoji => emoji.name === 'Peip'));
                 message?.react('🇦');
                 message?.react('🇧');
                 message?.react('🇨');
                 message?.react('🇩');
-                message?.react(message.guild.emojis.cache.find(emoji => emoji.name === 'Peip'));
+                message?.react(message.guild.emojis.cache.find(emoji => emoji.name === 'ESE'));
+                message?.react(message.guild.emojis.cache.find(emoji => emoji.name === 'AII'));
+                message?.react('⚡');
+                message?.react('👨‍🏭');
                 message?.react('🍺');
+                message?.react(message.guild.emojis.cache.find(emoji => emoji.name === 'Sportif'));
+                message?.react('🎵');
                 message?.react('🏨');
                 message?.react('🕵️‍♂️');
             });
         } catch (error) {}
+
+        interaction.deleteReply();
     }
 }

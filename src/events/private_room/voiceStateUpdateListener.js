2d0dbc124699d9d317349f433995a8cc14562b2f
Synol_ <louisvaillant44@gmail.com>
2022-08-21 19:59:23 +0200

    Update private room listeners

--- src/events/private_room/voiceStateUpdateListener.js
+++ src/events/private_room/voiceStateUpdateListener.js
@@ -9,30 +9,32 @@ module.exports = {
      * @param {VoiceState} newState
      */
     async run(oldState, newState) {
-        const db = newState.client.database.privateRooms
-        // leave
+        const db = newState.client.database.privateRooms;
+
+        // leave a voice channel
         if (newState.channelId === null) {
             if (!db.get('rooms')?.includes(oldState.channelId)) return;
             if (oldState.channel.members.size === 0) oldState.channel.delete(`[${newState.client.user.tag}] Delete {${newState.member.user.tag}}'s private room`)
         }
-        // join
+        // join a voice channel
         else {
-            if (oldState.channelId != null) {
-                if (db.get('rooms')?.includes(oldState.channelId) && oldState.channel.members.size === 0) s
-                    oldState.channel.delete(`[${newState.client.user.tag}] Delete {${newState.member.user.tag}}'s private room`)
+            // leave a room
+            if (oldState.channelId != null && db.get('rooms')?.includes(oldState.channelId)) oldState.channel.delete(`[${newState.client.user.tag}] Delete {${newState.member.user.tag}}'s private room`)
+
+            // join lobby
+            if (db.get('lobbies')?.includes(newState.channelId)) {
+                newState.guild
+                    .channels.create(`${newState.member.displayName}'s room`, {
+                        reason: `[${newState.client.user.tag}] Create private room for {${newState.member.user.tag}}`,
+                        type: 'GUILD_VOICE',
+                        userLimit: 20,
+                        parent: newState.channel.parent
+                    })
+                    .then(channel => {
+                        newState.member.voice.setChannel(channel);
+                        newState.client.database.privateRooms.push('rooms', channel.id)
+                    })
             }
-            if (!db.get('lobbies')?.includes(newState.channelId)) return;
-            newState.guild
-                .channels.create(`${newState.member.displayName}'s room`, {
-                    reason: `[${newState.client.user.tag}] Create private room for {${newState.member.user.tag}}`,
-                    type: 'GUILD_VOICE',
-                    userLimit: 20,
-                    parent: newState.channel.parent
-                })
-                .then(channel => {
-                    newState.member.voice.setChannel(channel);
-                    newState.client.database.privateRooms.push('rooms', channel.id)
-                })
         }
     }
 }
